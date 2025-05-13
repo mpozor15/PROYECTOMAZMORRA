f353330c72fb78128c9f4900915f79e7af919e68
manuel <manuelpozor2004@gmail.com>
2025-05-13 20:56:58 +0200

    arreglo moduleInfo

--- ProyectoMaz/src/main/java/module-info.java
+++ ProyectoMaz/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.ignaciomanuel {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.ignaciomanuel to javafx.fxml;
-    exports com.ignaciomanuel;
+    opens com.ignaciomanuel.mazmorra to javafx.fxml;
+    exports com.ignaciomanuel.mazmorra;
+
+    opens com.ignaciomanuel.mazmorra.logica to javafx.base;
+    exports com.ignaciomanuel.mazmorra.logica;
 }
