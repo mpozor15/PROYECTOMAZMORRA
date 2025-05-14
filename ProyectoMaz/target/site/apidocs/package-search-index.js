4be43ce2439b67c659b63af9c18473f16bf232bd
manuel <manuelpozor2004@gmail.com>
2025-05-14 14:36:40 +0200

    Javadoc completo

---
+++ ProyectoMaz/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.ignaciomanuel","l":"com.ignaciomanuel.mazmorra"},{"m":"com.ignaciomanuel","l":"com.ignaciomanuel.mazmorra.logica"}];updateSearchResults();
